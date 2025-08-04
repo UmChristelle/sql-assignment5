2b8180db1434aa295212aaaf8c6b4bcc86d796b9
Umchristella <umutonichristella17@gmail.com>
2025-08-04 10:27:31 +0200

    Added answers 3 and 4 to SQL assignment

--- assignment5.sql
+++ assignment5.sql
@@ -5,3 +5,5 @@ CREATE TABLE students (
     age INT
 );
 INSERT INTO students (id, name, age) VALUES (1, 'Alice', 22);
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
