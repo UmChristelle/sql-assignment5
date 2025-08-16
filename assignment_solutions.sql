bfc7b2bf7fa11bbea8e0fef556fa99bbe7873715
Umchristella <umutonichristella17@gmail.com>
2025-08-16 17:37:19 +0200

    Add SQL assignment solutions

---
+++ assignment_solutions.sql
@@ -0,0 +1,11 @@
+-- Question 1: Drop index IdxPhone from customers
+DROP INDEX IdxPhone ON customers;
+
+-- Question 2: Create user bob
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Question 3: Grant INSERT privilege to bob on salesDB
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+-- Question 4: Change bob's password
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
